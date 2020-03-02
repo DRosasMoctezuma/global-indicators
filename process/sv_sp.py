f40602d6d1a1482bb18143f269994d070b9d34b7
Carl Higgs <carl.higgs@rmit.edu.au>
2020-03-02 12:48:52 +1100

    revised full-distance-fix to reset index before writing to gpkg so as to retain point_id

--- process/sv_sp.py
+++ process/sv_sp.py
@@ -302,7 +302,7 @@ if __name__ == '__main__':
     samplePointsData[float_fields] = samplePointsData[float_fields].astype(float)
 
     # save the sample points with all the desired results to a new layer in geopackage
-    samplePointsData.to_file(
+    samplePointsData.reset_index().to_file(
         gpkgPath,
         layer=config["parameters"]["samplepointResult"]+'_revised',
         driver='GPKG')
